03b55002d3c5dfc4ee86af4828664de5607a0bc2
anilkrverma <anilv@twitter.com>
2020-03-22 22:47:27 +0530

    Added SkillSelectionActivity and intent to move to that activity

--- app/src/main/java/com/anil/playground/LeagueActivity.kt
+++ app/src/main/java/com/anil/playground/LeagueActivity.kt
@@ -1,12 +1,20 @@
 package com.anil.playground
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun nextButtonClicked(view: View){
+        var skillIntent = Intent(this, SkillSelectionActivity::class.java)
+        startActivity(skillIntent)
     }
 }
---
+++ app/src/main/java/com/anil/playground/SkillSelectionActivity.kt
@@ -0,0 +1,12 @@
+package com.anil.playground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillSelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill_selection)
+    }
+}
