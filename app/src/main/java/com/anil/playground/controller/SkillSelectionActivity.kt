2bf984286202ec9568952a7719d48977278f5fe0
anilkrverma <anilv@twitter.com>
2020-03-26 23:53:15 +0530

    Enhanced Activities to persist state while rotations.

--- app/src/main/java/com/anil/playground/controller/SkillSelectionActivity.kt
+++ app/src/main/java/com/anil/playground/controller/SkillSelectionActivity.kt
@@ -16,12 +16,24 @@ class SkillSelectionActivity : BaseActivity() {
 
     lateinit var player: Player
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill_selection)
         player = intent.getParcelableExtra(EXTRA_PLAYER)
 
         println(player.group)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null){
+            player = savedInstanceState.getParcelable<Player>(EXTRA_PLAYER)!!
+        }
     }
 
     fun onExpertBtnClicked(view: View){
